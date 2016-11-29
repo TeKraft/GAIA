c9d7f2d8ef078fd2041c7bdfce7021de0e2c9e4c
TeKraft <torbenEkraft@gmail.com>
2016-11-29 16:12:48 +0100

    style: bootstrap

--- server/server.js
+++ server/server.js
@@ -4,22 +4,32 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+// var R = require('r-script');
+
 var app = express();
-app.use(bodyParser.urlencoded({extended: true})); // to enable processing of the received post content
+/* get home page */
+app.use(express.static("../server"));
+app.use(express.static("../app"));
+// to enable processing of the received post content
+app.use(bodyParser.urlencoded({extended: true}));
 
 var config = {
-    httpPort: 9900,
+    httpPort: 8080,
     mongoPort: 27017
 }
 
 
 /* database schema */
 var profilSchema = mongoose.Schema({
-    vorname: String,
-    nachname: String,
-    email: String,
-    passwort: String,
-    instution: String,
+    // vorname: String,
+    // nachname: String,
+    // email: String,
+    // passwort: String,
+    // institution: String,
+
+    name: String,
+    dateInserted: Date,
+    data: {}
 });
 var Feature = mongoose.model('Feature', profilSchema);
 
@@ -48,7 +58,44 @@ app.use(function(req, res, next) {
 
 //get & post
 
+/* GET home page. */
+// app.get('/', function(req, res, next) {
+//     console.log("get home");
+//   res.render('login');
+//   // res.render('../app/html/login&registry');
+// });
 
+// respond with "hello world" when a GET request is made to the homepage
+// app.get('/', function(req, res) {
+//   res.send('hello world');
+// });
+
+// returns json of all stored features
+app.get('/getFeatures', function(req, res) {
+    Feature.find(function(error, features) {
+        if (error) return console.error(error);
+        res.send(features);
+    });
+});
+
+// takes a json document via POST, which will be added to the database
+// name is passed via URL
+// url format: /addFeature?name=
+app.post('/addFeature*', function(req, res) {
+	console.log(JSON.stringify(req.body));
+
+    var feature = new Feature({
+    	name: req.url.substring(17, req.url.length), // extract name from url
+    	dateInserted: new Date(),
+    	data: req.body
+    });
+    feature.save(function(error){
+        var message = error ? 'failed to save feature: ' + error
+                            : 'feature saved: ' + feature.name;
+        console.log(message + ' from ' + req.connection.remoteAddress);
+        res.send(message);
+    });
+});
 
 
 
