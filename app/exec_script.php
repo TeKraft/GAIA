c49a07d72c98ee1f286750ef6b7bb35d0108f3b3
Torben <t.kraf03@gmail.com>
2017-01-15 20:34:48 +0100

    Versuch03: opencpu

--- app/exec_script.php
+++ app/exec_script.php
@@ -1,13 +1,14 @@
 <?php
 
 	// echo "php script is echoing";
-	//
 	// phpinfo();
 	//
 	// exec("Rscript script.R $N", $callback);
 	// print_r($callback)
+	//
+	// echo "whatever";
 
-	echo "whatever";
+	print "hello world!";
 
 
 
