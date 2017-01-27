a49353db5b4977b817e6a75709a6c4c54fbb3ec7
TeKraft <torbenEkraft@gmail.com>
2017-01-27 23:13:56 +0100

    fix leafletDraw, create bbox
    popup shows bbox - for further development
    fixed leafletDraw by comment in work.css

--- app/js/mapBuilder.js
+++ app/js/mapBuilder.js
@@ -13,9 +13,9 @@
  */
 
 var leafmap;
-var myLayerControl;
-
-
+var customPopup;
+var drawnItems;
+var drawControlFull;
 
 /**
  * Function creates map containing base layer.
@@ -49,28 +49,14 @@ var leaflet = function () {
 	var overlayMaps = {};
 
 	// layer control function
-	myLayerControl = L.control.layers(baseMaps, overlayMaps).setPosition('topright').addTo(leafmap);
-
-  
-
+	var myLayerControl = L.control.layers(baseMaps, overlayMaps).setPosition('topright').addTo(leafmap);
 }; //leaflet() end
-
-
-var customPopup;
-var rectangleOptions;
-var drawnItems;
-var drawControlFull;
-var drawControlEditOnly;
-
-var latlon;
 
 /**
  * @desc Method to draw rectangle on map
  *
  */
-var drawMap = function () {
-	// JL("function: ").info("drawMap");
-
+function drawMap() {
 	// Initialise the FeatureGroup to store editable layers
 	drawnItems = new L.FeatureGroup();
 	leafmap.addLayer(drawnItems);
@@ -80,27 +66,17 @@ var drawMap = function () {
 
 	// create control to draw rectangle
 	drawControlFull = new L.Control.Draw({
-		// edit: {
-		// 	featureGroup: drawnItems
-		// },
-			edit: false,
+    edit: false,
 		position: 'topleft',
 		draw: {
 			polyline: false,
 			polygon: false,
 			rectangle: {
-				title: 'new rectangle',
-				// allowIntersection: false,
-				// drawError: {
-				// 	color: 'orange',
-				// 	timeout: 1000
-				// },
+				title: 'new bbox',
 				shapeOptions: {
 					color: 'purple',
 					clickable: true
 				},
-
-				// showArea: true,
 				repeatMode: false
 			},
 			circle: false,
@@ -108,98 +84,42 @@ var drawMap = function () {
 		}
 	});
 
-	// create control where draw is disabled
-	drawControlEditOnly = new L.Control.Draw({
-		edit: {
-			featureGroup: drawnItems
-		},
-		position: 'topleft',
-		draw: false
-	});
-
 	// add control with draw abled
 	leafmap.addControl(drawControlFull);
 
 	// specify popup options
-	rectangleOptions =
+	 var rectangleOptions =
 		{
 		'maxWidth': '500',
 		'className' : 'custom'
 	};
 
-
 	// for creating
 	leafmap.on("draw:created", function (e) {
 		drawnItems.clearLayers();
-		// clearAllFields(rectangleview);
 
-		// // check if rectangle has been drawn before
-		// if (rectangleOnLoad !== undefined) {
-		// 	leafmap.removeLayer(rectangleOnLoad);
-		// }
 		var type = e.layerType,
 			layer = e.layer;
 		// get marker options
-		customPopup = "new rectangle created";
+		customPopup = "new coordinates choosen";
 		if (type === 'rectangle') {
 			layer.bindPopup(customPopup, rectangleOptions);
 		}
-		console.log(drawnItems);
-		console.log(drawnItems._layers);
-		console.log("create");
-		console.log(layer._latlngs);
-		layer.addTo(drawnItems);
 
-		// // manipulate rectangle-value for editing / saving new rectangle
-		// manipulaterectangle(drawnItems);
+    var xmin = layer._latlngs[0].lat;
+    var xmax = layer._latlngs[2].lat;
+    var ymin = layer._latlngs[0].lng;
+    var ymax = layer._latlngs[2].lng;
 
-		// after draw set draw control to disabled
-		drawControlFull.removeFrom(leafmap);
-		drawControlEditOnly.addTo(leafmap)
+    var bounds = L.latLngBounds(layer._latlngs[2], layer._latlngs[0]);
+    console.log(bounds);
+    var bbox = "bbox1 = extent("+xmin+","+xmax+","+ymin+","+ymax+")\ndata.subarea = crop(x = ..., bbox1) # please add the dataset here";
+    window.alert(bbox);
+  	layer.addTo(drawnItems);
+
 	});
-
-
-	// if rectangle has been deleted
-	leafmap.on("draw:deleted", function(e) {
-		drawControlEditOnly.removeFrom(leafmap);
-		// clearAllFields(rectangleview);
-		// if (rectangleOnLoad !== undefined) {
-		// 	document.getElementById('rectangle-view').value = thisrectangle;
-		// 	leafmap.addLayer(rectangleOnLoad);
-		// }
-		console.log("delete");
-		drawControlFull.addTo(leafmap);
-	});
-
-	// // if rectangle has been edited
-	// leafmap.on("draw:edited", function(e) {
-	//
-	// 	var layers = e.layers;		// rectangle before editing
-	// 	var layer = e.layer;
-	// 	console.log("layers");
-	// 	console.log(layers);			// rectangle before editing
-	// 	console.log("layer");
-	// 	console.log(layer);				// = undefined
-	// 	//
-	// 	// 	// get marker options
-	// 	// 	customPopup = "new rectangle created";
-	// 	// 	if (type === 'rectangle') {
-	// 	// 		layer.bindPopup(customPopup, rectangleOptions);
-	// 	// 	};
-	//
-	// 	console.log("edit");
-	// 	console.log(drawnItems);
-	//
-	// 	console.log(layers._latlngs);
-	//
-	// 	// latlng = drawnItems;
-	//
-	// 	// console.log(layer._latlngs);
-	// });
-
 }; //drawMap() end
 
 
 	leaflet();
 	drawMap();
-
