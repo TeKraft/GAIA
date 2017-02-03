0b0db087ef93c2555c87b10b29a3e0fd0f0cb036
viktorgorte <v_gort01@uni-muenster.de>
2017-02-03 17:21:21 +0100

    import from Results via iFrame & Path-input

--- app/js/executeScript.js
+++ app/js/executeScript.js
@@ -18,21 +18,12 @@ function getMap() {
   $("#leafletmap").fadeIn(1000);
   leaflet();
 }
-// entweder importHTML oder createiframe --- nicht beide nötig!
-
-function importHTML() {
-  leafmap.remove();
-  var html = document.getElementById("htmlPath");
-  var objectURL = html.value;
-  document.getElementById("leafletmap").innerHTML ='<object type="text/html" height="100%" width="100%" data="' + objectURL + '" ></object>';
-  console.log(document.getElementById("leafletmap"));
-}
 
 // entweder importHTML oder createiframe --- nicht beide nötig!
 function createiframe() {
   leafmap.remove();
   var html = document.getElementById("htmlPath");
-  var objectURL = html.value;
+  var objectURL = "../projects/" + html.value;
   document.getElementById("leafletmap").innerHTML = '<iframe id="iframeMap" src="' + objectURL + '" height="100%" width="100%" name"myIframe"></iframe>';
   console.log("iframe data");
   console.log($('#leafletmap').contents());
