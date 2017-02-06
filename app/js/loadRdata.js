1d7a311fbd00d2c5a13e5a970f722754e611178c
viktorgorte <v_gort01@uni-muenster.de>
2017-02-06 01:48:58 +0100

    comments

--- app/js/loadRdata.js
+++ app/js/loadRdata.js
@@ -1,10 +1,13 @@
-
+/**
+  * @desc Function for reading a CSV file and writing it to a div on the work.html
+*/
 function readCSV() {
+  //resetting the content of the div so that there is only 1 table at a time
   document.getElementById("wrap").innerHTML = "";
-
   $.get('../scriptsR/datasets.csv', function(data) {
   	var build = '<table border="1" cellpadding="2" cellspacing="0" style="border-collapse: collapse" width="100%">\n';
   	var rows = data.split("\n");
+    //building the table
   	rows.forEach( function getvalues(thisRow) {
     	build += "<tr>\n";
     	var columns = thisRow.split(",");
@@ -15,6 +18,4 @@ function readCSV() {
   	$('#wrap').append(build);
     window.location.href = "#wrap";
    });
-
-
 }
